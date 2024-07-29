e1f4d4f96d3783f2c355b6dc1518d85cf5f13709
Uzziel Gonzalez <uzzielgonzalez247@gmail.com>
2024-07-29 19:26:47 -0400

    Set up documentation

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 
+//Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const authentication = require('./middleware/authentication');
 //connectDB
 const connectDB = require('./db/connect');
@@ -31,6 +36,10 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 // extra packages
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1> <a href = "/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
